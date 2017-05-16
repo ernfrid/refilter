a85c4bf5960be5c4347de72604ee269b4583e6df
Dave Larson <dlarson@genome.wustl.edu>
2017-05-16 13:02:53 -0500

    update test data

--- tests/test_refilter.py
+++ tests/test_refilter.py
@@ -10,8 +10,11 @@ class TestFilter(unittest.TestCase):
         # store each variant object into specific variables for tes
         test_directory = os.path.dirname(os.path.abspath(__file__))
         reader = VCF(os.path.join(test_directory, "test.vcf"))
+        self.test_filter = refilter.Filter(0.3, 0.7, 'AB', 'VAR_DP', 5, -5.0, ['MISSING'], ['DB'])
+        reader.add_filter_to_header(self.test_filter.filtered_header())
+        reader.add_info_to_header(self.test_filter.rescued_header())
+
         self.variants = [ variant for variant in reader ]
-        self.test_filter = refilter.Filter(0.3, 0.7, 'AB', 5, -5.0, ['MISSING'], ['DB'])
 
     def test_filters_ok(self):
         self.assertTrue(self.test_filter.filters_ok(self.variants[0]))
@@ -28,7 +31,13 @@ class TestFilter(unittest.TestCase):
             self.assertEqual(variant.FILTER, None)
 
     def test_rescue(self):
-        expected_filters = (None, None, 'VQSRTrancheSNP99.00to99.90;MISSING', 'VQSRTrancheSNP99.00to99.90', None)
+        expected_filters = (
+                self.test_filter.filter_tag,
+                self.test_filter.filter_tag,
+                ';'.join(('VQSRTrancheSNP99.00to99.90;MISSING', self.test_filter.filter_tag)),
+                ';'.join(('VQSRTrancheSNP99.00to99.90', self.test_filter.filter_tag)),
+                None
+                )
         for index, variant in enumerate(self.variants):
             self.test_filter.rescue(variant)
             self.assertEqual(variant.FILTER, expected_filters[index])
